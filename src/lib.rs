e0c38be659a6150876436ad4fc8a7087d1b3a4fd
Vasyl <vasylcode@gmail.com>
2023-10-13 16:32:18 -0600

    lib added

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use async_graphql::{Request, Response, SimpleObject};
+use linera_sdk::{
+    base::{ContractAbi, ServiceAbi, ChainId, Owner, Timestamp},
+    graphql::GraphQLMutationRoot,
+};
+use serde::{Deserialize, Serialize};
+
+pub struct ApplicationAbi;
+
+impl ContractAbi for ApplicationAbi {
+    type Parameters = ();
+    type InitializationArgument = ();
+    type Operation = Operation;
+    type Message = ();
+    type ApplicationCall = ();
+    type SessionCall = ();
+    type SessionState = ();
+    type Response = ();
+}
+
+impl ServiceAbi for ApplicationAbi {
+    type Parameters = ();
+    type Query = Request;
+    type QueryResponse = Response;
+}
+
+#[derive(PartialEq, Debug, Clone, Serialize, Deserialize, SimpleObject)]
+pub struct User {
+    pub username: String,
+    pub owner: Owner,
+    pub chain_id: ChainId,
+    pub timestamp: Timestamp,
+}
+
+#[derive(Debug, Deserialize, Serialize, GraphQLMutationRoot)]
+pub enum Operation {
+    SetUser {
+        username: String,
+        // owner: Owner,
+        // chain_id: ChainId,
+        // timestamp: Timestamp,
+    },
+}
