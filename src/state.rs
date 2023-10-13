a10d53c59015df7dd4073beb1d7cbfb95edc084a
Vasyl <vasylcode@gmail.com>
2023-10-13 16:32:26 -0600

    state added

---
+++ src/state.rs
@@ -0,0 +1,35 @@
+// use linera_sdk::views::{ViewStorageContext, LogView};
+use linera_sdk::views::{ViewStorageContext, MapView};
+use linera_views::views::{GraphQLView, RootView};
+use linera_link::{User};
+use thiserror::Error;
+
+#[derive(RootView, GraphQLView)]
+#[view(context = "ViewStorageContext")]
+pub struct Application {
+    pub users: MapView<String, User>,
+    // pub users: LogView<User>,
+}
+
+#[allow(dead_code)]
+impl Application {
+
+    pub(crate) async fn set_user(
+    &mut self,
+    user: User,
+    ) -> Result<(), StateError> {
+        match self.users.insert(&user.clone().username, user) {
+            Ok(_) => Ok(()),
+            Err(err) => Err(StateError::ViewError(err)),
+        }
+    }
+
+}
+
+
+#[derive(Debug, Error)]
+#[error("test1")]
+pub enum StateError {
+    #[error("View error")]
+    ViewError(#[from] linera_views::views::ViewError),
+}
