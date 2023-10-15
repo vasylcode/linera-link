b9f82baf7d008ed3d2da7acbfb127ec9dfd0b141
Vasyl <vasylcode@gmail.com>
2023-10-14 21:35:52 -0600

    profile query added

--- frontend/pages/[username].js
+++ frontend/pages/[username].js
@@ -14,12 +14,27 @@ const GET_USER = gql`
 
 export default function Profile() {
 	const router = useRouter();
-	const { username, test } = router.query;
-	console.log(test);
+	const { username } = router.query;
+
+	const { loading, error, data } = useQuery(GET_USER, {
+		variables: { username },
+	});
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error: {error.message}</p>;
+
+	const user = data.users;
 
 	return (
 		<div>
 			<h1>{`Welcome ${username}`}</h1>
+			<div>
+				<h1>User Profile</h1>
+				<p>Username: {user.username}</p>
+				<p>Chain ID: {user.chainId}</p>
+				<p>Owner: {user.owner}</p>
+				<p>Timestamp: {user.timestamp}</p>
+			</div>
 		</div>
 	);
 }
