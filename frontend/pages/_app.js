29cd6c051cc8f3204e87926ddb980cee53b5f4c3
Vasyl <vasylcode@gmail.com>
2023-10-15 14:48:59 -0600

    port & vars changed

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,12 +5,18 @@ import { useEffect } from "react";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import { ApolloProvider } from "@apollo/client";
-import client from "./lib/apollo";
+import createApolloClient from "./lib/apollo";
+import { useSearchParams } from "next/navigation";
 
 export default function App({ Component, pageProps }) {
 	useEffect(() => {
 		import("preline");
 	}, []);
+
+	const searchParams = useSearchParams();
+	let port = searchParams.get("port") ?? 8080;
+
+	const client = createApolloClient(port);
 
 	return (
 		<ApolloProvider client={client}>
--- frontend/pages/lib/apollo.js
+++ frontend/pages/lib/apollo.js
@@ -1,26 +1,34 @@
 import { ApolloClient, InMemoryCache, HttpLink, split } from "@apollo/client";
 
-const LINK_CHAIN_ID = process.env.NEXT_PUBLIC_LINK_CHAIN_ID;
+const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID;
 const LINK_APP_ID = process.env.NEXT_PUBLIC_LINK_APP_ID;
-const LINK_PORT = process.env.NEXT_PUBLIC_LINK_PORT;
-
-const FUNGIBLE_CHAIN_ID = process.env.NEXT_PUBLIC_FUNGIBLE_CHAIN_ID;
 const FUNGIBLE_APP_ID = process.env.NEXT_PUBLIC_FUNGIBLE_APP_ID;
-const FUNGIBLE_PORT = process.env.NEXT_PUBLIC_FUNGIBLE_PORT;
 
-// linera-link app
-const httpLink = new HttpLink({
-	uri: `http://localhost:${LINK_PORT}/chains/${LINK_CHAIN_ID}/applications/${LINK_APP_ID}`,
-});
+const createApolloClient = (port) => {
+	// linera-link app
+	const httpLink = new HttpLink({
+		uri: `http://localhost:${port}/chains/${CHAIN_ID}/applications/${LINK_APP_ID}`,
+	});
 
-// fungible app
-const fungibleLink = new HttpLink({
-	uri: `http://localhost:${FUNGIBLE_PORT}/chains/${FUNGIBLE_CHAIN_ID}/applications/${FUNGIBLE_APP_ID}`,
-});
+	// fungible app
+	const fungibleLink = new HttpLink({
+		uri: `http://localhost:${port}/chains/${CHAIN_ID}/applications/${FUNGIBLE_APP_ID}`,
+	});
 
-const client = new ApolloClient({
-	link: split((operation) => operation.getContext().clientName === "fungible", fungibleLink, httpLink),
-	cache: new InMemoryCache(),
-});
+	// clear link query
+	const clearLink = new HttpLink({
+		uri: `http://localhost:${port}`,
+	});
 
-export default client;
+	return new ApolloClient({
+		// link: split((operation) => operation.getContext().clientName === "fungible", fungibleLink, httpLink),
+		link: split(
+			(operation) => operation.getContext().clientName === "clear",
+			clearLink,
+			split((operation) => operation.getContext().clientName === "fungible", fungibleLink, httpLink)
+		),
+		cache: new InMemoryCache(),
+	});
+};
+
+export default createApolloClient;
